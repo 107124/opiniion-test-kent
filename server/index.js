f61326058ad07e9753fc3959e2b0282815a8189b
107124 <kredfox4@gmail.com>
2022-09-07 13:39:49 -0400

    initial commit

--- server/index.js
+++ server/index.js
@@ -12,14 +12,6 @@ const customers = require('./routes/api/customers');
 
 app.use('/api/customers', customers);
 
-// // Handle production
-// if (process.env.NODE_ENV === 'production') {
-//   // Static folder
-//   app.use(express.static(__dirname + '/public/'));
-
-//   // Handle SPA
-//   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-// }
 
 const port = process.env.PORT || 5050;
 
--- server/routes/api/customers.js
+++ server/routes/api/customers.js
@@ -1,30 +1,45 @@
+const { text } = require('body-parser');
 const express = require('express');
 const mongodb = require('mongodb');
 
 const router = express.Router();
 
+function subtractWeeks(numOfWeeks, date = new Date()) {
+    date.setDate(date.getDate() - numOfWeeks * 7);
+
+    return date;
+  }
+
 // Get customers
 router.get('/', async (req, res) => {
     const customers = await loadCustomersCollection();
-      res.send(await customers.find({}).toArray());
+    res.send(await customers.find({}).toArray());
     // res.send("Hello")
 });
 
-// Add Post
+// Add Post locations
 router.post('/', async (req, res) => {
     const customers = await loadCustomersCollection();
     await customers.insertOne({
-        text: req.body.text,
-        createdAt: new Date(),
+        locationId: req.params.id,
+        name: text,
+        startDate: subtractWeeks(Math.random(), new Date()),
+        endDate: subtractWeeks(Math.random(), new Date())
+        // _id:6317716985645cc431b4f7da
+        // firstName:"kent"
+        // phone:"555-999-7898"
+        // lastName:"taylor"
+        // email:"kredfox4@gmail.com"
 
     });
-    res.status(201).send();
+    res.status(201).send("Finished!");
 });
 
 // Delete Post
 router.delete('/:id', async (req, res) => {
     const customers = await loadCustomersCollection();
-    await customers.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+    await customers.deleteOne({ _id: mongodb.ObjectId(req.params.id)});
+    console.log(mongodb.ObjectId(req.params.id))
     res.status(200).send({});
 });
 
